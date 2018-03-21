81f719df50dddceb92f14a2fb42e69fdf3459a07
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-03-21 22:36:36 +0100

    store changes

--- src/store.js
+++ src/store.js
@@ -1,25 +1,15 @@
-import {
-    createStore,
-    combineReducers,
-    applyMiddleware,
-    compose
-} from 'redux'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import movies from './state/movies'
 
-
-
-const reducer = combineReducers({
-
+const reducers = combineReducers({
+    movies
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+export const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(thunk))
+)
 
-
-const store = createStore(
-    reducer,
-    )
-
-
-
-export default store
