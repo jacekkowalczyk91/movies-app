7f08fb59013f87df1b58cb12fc05d3258a025c86
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-03-22 18:48:36 +0100

    Provider + store imported

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
-import Movies from "./Components/Movies/Movies";
+import './App.css'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Movies from './Components/Movies'
+
 
 class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <Movies/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div>
+                        <Route exact path="/" component={Movies}/>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
