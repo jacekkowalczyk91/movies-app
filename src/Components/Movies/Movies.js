934e9e4de00f50886be4593266a0ac4b84313aba
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-03-25 13:14:48 +0200

    movies css added

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import './Movies.css'
 
 
 
@@ -9,11 +10,12 @@ class Movies extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className='movies-view'>
                 {
                     this.props.moviesData && this.props.moviesData.map((movie, index) => (
-                        <div key={index}>
+                        <div className='movie' key={index}>
                             <h5>{movie.Title}</h5>
+                            <img src={movie.Poster} alt={movie.Title}/>
                             <Link to={`/movie/${movie.imdbID}`}>
                                 <button>More</button>
                             </Link>
